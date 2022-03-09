338a814d0e2718bffbd1fdbdf5364d7a9b6d0f1d
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2022-03-09 18:30:44 +0300

    Finalisation du script

--- front/js/script.js
+++ front/js/script.js
@@ -1,25 +1,26 @@
-
-// Debut      ----- API et Affichage sur le site ------
+// ----- API et Affichage sur le site ------
 // Lancement de l'API dans le dossier back> npm install 
 //                                    back> node server
+//                                      port 3000
 // Adresse de l'API
 const httpAPI = 'http://localhost:3000/api/products/';
 
 // Requete à l'API pour l'affichage en console et la récupération du JSON
 async function recuperationArticles() {
   return await fetch(httpAPI)
-    .then(function (reponse) {
+    .then(function(reponse) {
       console.log("l'API a bien repondu");
       return reponse.json();
     })
-    .then(function (value) {
+    .then(function(value) {
         return value;
     })
-    .catch(function (err) {
+    .catch(function(err) {
       console.log('Erreur dans le fetch');
       console.log(err);
     });
 }
+
 
 // Récupération des données et intégration dans le DOM
 async function articlesAPI() {
@@ -32,16 +33,17 @@ async function articlesAPI() {
 
   // Boucle de l'ensemble des produits et affichage dans le DOM
   for (i = 0; i < products.length; i++) {
-    let gabaritHTML = `
-      <a href="./product.html?id=${products[i]._id}">
+    let gabaritHTML = 
+    `<a href="./product.html?id=${products[i]._id}">
         <article id="element${i}">
           <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
           <h3 class="productName">${products[i].name}</h3>
           <p class="productDescription">${products[i].description}</p>
         </article> 
-      </a>
-      `;
+      </a>`;
+
       console.log("Le produit a l'index " + i + " est affiché");
+
     // Analyse une chaîne de caractères et retourne un HTMLDocument.
     const affichageArticles = analyser.parseFromString(gabaritHTML, "text/html");
     blocHTMLArticle.appendChild(affichageArticles.body.firstChild);
@@ -50,23 +52,3 @@ async function articlesAPI() {
 
 // Appel de la fonction
 articlesAPI();
-// Fin     ----- API et Affichage sur le site ------
-
-
-// Debut      ----- Evenement et recuperation  ------
-
-function test1() {
-
-  let elementAclick = document.getElementById('element1');
-
-  elementAclick.addEventListener('mouseenter', function() {
-
-  console.log('Yes');
-
-  });
-
-
-}
-
-setTimeout(test1, 5000);
-// Fin      ----- Evenement et recuperation  ------
